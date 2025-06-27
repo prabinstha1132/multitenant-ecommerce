d233129a79a58a7f1acb68cfed3f2b0f804e9bfa
Sumita Lama <sumitalama@Sumitas-MacBook-Pro-2.local>
2025-06-26 20:39:55 -0400

    update

--- Desktop/multitalent-ecommerce/src/app/page.tsx
+++ Desktop/multitalent-ecommerce/src/app/page.tsx
@@ -1,9 +1,30 @@
+import { Input } from "@/components/ui/input"
 import {Button} from "../components/ui/button"
+import { Progress } from "@/components/ui/progress"
+import { Textarea } from "@/components/ui/textarea"
+
+
 
 export default function Home() {
  return (
-  <Button className="text-red-300">
-hello world
+  <div className="p-4">
+  <div className ="flex flex-col gap-y-4">
+    <div>
+  <Button variant="elevated" className="text-blue-300">
+Home
   </Button>
+  </div>
+  <div>
+    <Input placeholder="name"
+    name="title"/>
+  </div>
+  <div>
+    <Progress value={50}/>
+  </div>
+  <div>
+    <Textarea placeholder="write feedback"/>
+  </div>
+  </div>
+  </div>
  )
 }
--- Desktop/multitalent-ecommerce/src/components/ui/progress.tsx
+++ Desktop/multitalent-ecommerce/src/components/ui/progress.tsx
@@ -15,13 +15,15 @@ function Progress({
       data-slot="progress"
       className={cn(
         "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
+        //modified classess
+        "border bg-transparent",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
+        className="bg-pink-300 h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
